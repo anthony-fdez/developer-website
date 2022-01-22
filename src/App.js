88754339fa4b89ef166b638d2e4d6cc694bee9ad
Anthony <anthonyfdez23@gmail.com>
2022-01-21 23:13:11 -0500

    Added basketball center to the work page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 
 // Components
 import Header from "./components/header/header";
+import Work from "./components/work/work";
 
 function App() {
   return (
@@ -9,6 +10,27 @@ function App() {
       <header className="App-header">
         <Header />
       </header>
+      <main>
+        <div className="about-jumbo">
+          <h3>Hi, I'm Anthony! 👋</h3>
+          <h1>
+            I'm a fullstack developer. Focused on building better apps every
+            day.
+          </h1>
+          <p></p>
+          <a
+            target="_blank"
+            href="mailto:anthonyfdez23@gmail.com"
+            className="main-button"
+            rel="noreferrer"
+          >
+            Contact Me
+          </a>
+        </div>
+        <br></br>
+        <h2 className="work-title">Work</h2>
+        <Work />
+      </main>
     </div>
   );
 }
