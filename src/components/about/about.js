98103283f5c436f2051ad4b971c7907f0a5b92c5
Anthony <anthonyfdez23@gmail.com>
2022-03-17 17:14:27 -0400

    added the sklls tab

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./about.css";
 
 const About = () => {
-  return (
-    <div className="about-container">
-      <h1>Abbout Me</h1>
-    </div>
-  );
+  return <div className="about-container">{/* <h1>Abbout Me</h1> */}</div>;
 };
 
 export default About;
--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -3,8 +3,58 @@ import "./skills.css";
 
 const Skills = () => {
   return (
-    <div>
+    <div id="content">
       <h1>Skills</h1>
+      <br></br>
+      <div className="columns">
+        <ul>
+          <li>
+            <p>React</p>
+          </li>
+          <li>
+            <p>React Native</p>
+          </li>
+          <li>
+            <p>NodeJS</p>
+          </li>
+          <li>
+            <p>JavaScript</p>
+          </li>
+          <li>
+            <p>HTML5</p>
+          </li>
+          <li>
+            <p>CSS</p>
+          </li>
+          <li>
+            <p>REST APIs</p>
+          </li>
+          <li>
+            <p>JSON</p>
+          </li>
+          <li>
+            <p>Redux</p>
+          </li>
+          <li>
+            <p>Git</p>
+          </li>
+          <li>
+            <p>Linux</p>
+          </li>
+          <li>
+            <p>Express JS</p>
+          </li>
+          <li>
+            <p>UI Design</p>
+          </li>
+          <li>
+            <p>Bootstrap</p>
+          </li>
+          <li>
+            <p>Fluent in Spanish</p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
