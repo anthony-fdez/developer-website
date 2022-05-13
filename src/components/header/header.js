0cb43728359a70f6a66a8f171a1dcf4df0ba03a6
Anthony Fernandez <anthonyfdez23@gmail.com>
2022-05-13 17:50:16 -0400

    Fixed header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -11,13 +11,30 @@ const Header = () => {
     <div className="header-container">
       <div className="header-content">
         <span className="header-title">Portfolio.</span>
-        <div>
+        <nav>
           <ul className="navigation-list" role="navigation">
-            <li onClick={() => scrollTo({ elementId: "work" })}>Work</li>
-            <li onClick={() => scrollTo({ elementId: "about" })}>About</li>
-            <li onClick={() => scrollTo({ elementId: "contact" })}>Contact</li>
+            <li>
+              <button
+                className="navigation_button"
+                onClick={() => scrollTo({ elementId: "work" })}
+              >
+                Work
+              </button>
+            </li>
+            <button
+              className="navigation_button"
+              onClick={() => scrollTo({ elementId: "about" })}
+            >
+              About
+            </button>
+            <button
+              className="navigation_button"
+              onClick={() => scrollTo({ elementId: "contact" })}
+            >
+              Contact
+            </button>
           </ul>
-        </div>
+        </nav>
       </div>
     </div>
   );
